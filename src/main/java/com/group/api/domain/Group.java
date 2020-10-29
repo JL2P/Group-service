dfe600c90c79411034c910613bc7536e56a100eb
2myungho <dl_audgh@naver.com>
2020-10-29 23:41:13 +0900

    Group.java에 Setter 추가

--- src/main/java/com/group/api/domain/Group.java
+++ src/main/java/com/group/api/domain/Group.java
@@ -3,12 +3,14 @@ package com.group.api.domain;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
+@Setter
 @Entity
 @Table(name = "groups")
 public class Group {
@@ -20,5 +22,6 @@ public class Group {
     private String category;    // 카테고리
     private String master;      // 그룹장ID
     private String description; // 설명글
+    private String imgUrl;      // 이미지 URL
 
 }
