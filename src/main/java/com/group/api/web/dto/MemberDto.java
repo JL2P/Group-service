6cc3bd66020702d0ee729d2897fd5343e57e808e
2myungho <dl_audgh@naver.com>
2020-11-02 22:22:01 +0900

    memberDto 설정

--- src/main/java/com/group/api/web/dto/MemberDto.java
+++ src/main/java/com/group/api/web/dto/MemberDto.java
@@ -1,5 +1,36 @@
 package com.group.api.web.dto;
 
+import org.springframework.beans.BeanUtils;
+
+import com.group.api.domain.Group;
+import com.group.api.domain.Member;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
 public class MemberDto {
-    // 이건 만들어야할듯...
+	
+	private Long id;
+	private String accountId;   // 멤버 ID
+    private String confirm;     // (승인 Y / 미승인 N) 승인여부
+    private Long groupId;
+    private String manager; //관리자
+    
+    public MemberDto(Member member){
+    	BeanUtils.copyProperties(member, this);
+    }
+    
+    public Member toDomain() {
+    	Member member = new Member();
+    	BeanUtils.copyProperties(this,member);
+    	return member;
+    }
 }
