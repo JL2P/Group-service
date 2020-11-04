f2f32048aa23a082f105f564fb57868e072f9f3b
LeeMH123 <sky85210@gmail.com>
2020-11-04 23:52:15 +0900

    Member.java에 setter 추가

--- src/main/java/com/group/api/domain/Member.java
+++ src/main/java/com/group/api/domain/Member.java
@@ -3,12 +3,14 @@ package com.group.api.domain;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
+@Setter
 @Entity
 @Table(name = "members")
 public class Member {
