795d3323eb61a86116225a4d322ba97897c6e471
2myungho <dl_audgh@naver.com>
2020-11-05 18:18:34 +0900

    주석 추가

--- src/main/java/com/group/api/domain/Member.java
+++ src/main/java/com/group/api/domain/Member.java
@@ -20,7 +20,7 @@ public class Member {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "group_id", nullable = false)
-    private Group group;        // Group ID
+    private Group group;        // Group ID //group 객체를 받아오는데 자동으로 id를 보여준다.
 
     private String accountId;   // 멤버 ID
     private String confirm;     // (승인 Y / 미승인 N) 승인여부
